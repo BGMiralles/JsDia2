46f9bef44eece9a70bf922947a1a502a6c962c36
Borja <pintocorreas@gmail.com>
2023-09-21 12:15:06 +0200

    Funcion Sumar y Saludo

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+// function Sumar(numero1, numero2){
+
+//     return numero1 + numero2;
+// }
+
+// const resultado = Sumar(5, 6)
+
+// console.log(resultado)
+
+// function Saludo(nombre){
+//     return console.log(`Hola ${nombre}`)
+// }
+
+// Saludo("Borja")
+
+nombreUsuario = prompt("Dime tu nombre").toString()
+
+function Saludo(nombre){
+    return console.log(`Hola ${nombre}`)
+}
+
+
+Saludo(nombreUsuario)
